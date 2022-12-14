89711f0991263837655f33e968095773bd4293b5
Lanfear7 <68174240+Lanfear7@users.noreply.github.com>
2022-12-13 19:00:08 -0800

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-const port = 80
+const port = 8080
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
